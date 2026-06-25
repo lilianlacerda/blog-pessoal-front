dc8de12cc52cfa85284936cee3d174b48ead5bc8
lilianlacerda <lilianraiany@gmail.com>
2026-06-25 15:02:59 -0300

    inclusão rota login

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Cadastro from "./pages/cadastro/Cadastro";
+import Login from "./pages/login/Login";
 
 function App() {
   return (
@@ -11,8 +12,9 @@ function App() {
         <Navbar />
         <div className="min-h-[80vh]">
           <Routes>
+            <Route path="/" element={<Login />} />
             <Route path="/home" element={<Home />} />
-            <Route path="/" element={<Cadastro />} />
+            <Route path="/cadastro" element={<Cadastro />} />
           </Routes>
         </div>
         <Footer />
