5ad6d5d86e5fefd35684555a88bfc92ac54c2061
lilianlacerda <lilianraiany@gmail.com>
2026-07-01 07:42:58 -0300

    criação de CRUD

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -4,15 +4,17 @@ const api = axios.create({
   baseURL: 'https://blogpessoal-x4ui.onrender.com'
 })
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
 export const cadastrarUsuario = async (url: string, dados: object, setDados: Function) => {
   const resposta = await api.post(url, dados)
   setDados(resposta.data)
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
 export const login = async (url: string, dados: object, setDados: Function) => {
   const resposta = await api.post(url, dados)
   setDados(resposta.data)
 }
 
+export const buscar = async(url: string, setDados: Function, header: Object) => {
+  const resposta = await api.get(url, header)
+  setDados(resposta.data)
+}
